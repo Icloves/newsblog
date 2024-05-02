d2a15a8e5674d63dd415a137fe509313db874cd6
Ilya Osipov <ilyaoa19@gmail.com>
2024-05-02 13:56:42 +0300

    update Navbar

--- client/src/components/ui/Navigation.jsx
+++ client/src/components/ui/Navigation.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { CgProfile } from 'react-icons/cg';
+// import User from '../../../../server/db/models/user'
 
 export default function Navigation() {
-  const user = 'Ф';
+  const user = '1';
   return (
-    <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="justify-content-between">
+    <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="justify-content-between" mb="30px">
       <Container>
         <Navbar.Brand href="/">{user ? `Hello, ${user}` : 'Guest'}</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/">Home</Nav.Link>
         </Nav>
         {user ? (
-          <Nav>
-            <Nav.Link as={Link} to="/account">Account</Nav.Link>
-            <Button>Logout</Button>
-          </Nav>
+          <Nav />
         ) : (
           <Nav>
             <Nav.Link as={Link} to="/login">Login</Nav.Link>
             <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
           </Nav>
         )}
-        <NavDropdown title="Профиль" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">
-            Another action
+        <NavDropdown
+          title={(
+            <span>
+              <CgProfile size={30} />
+            </span>
+)}
+          id="basic-nav-dropdown"
+          style={{ color: 'white' }}
+        >
+          <NavDropdown.Item href="/">Избранное</NavDropdown.Item>
+          <NavDropdown.Item href="/">
+            Прочитанное
           </NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Item href="/">Настройки</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">
-            Separated link
+          <NavDropdown.Item href="/">
+            Выйти
           </NavDropdown.Item>
         </NavDropdown>
-
       </Container>
     </Navbar>
   );
